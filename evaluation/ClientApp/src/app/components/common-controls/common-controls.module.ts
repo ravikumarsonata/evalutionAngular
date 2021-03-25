2597dc5c3b23171b90a59b753c63b20fe23c686d
Ravikumar <ravikumarachari.s@sonata-software.com>
2021-03-25 13:18:59 +0530

    added controls

--- evaluation/ClientApp/src/app/components/common-controls/common-controls.module.ts
+++ evaluation/ClientApp/src/app/components/common-controls/common-controls.module.ts
@@ -9,10 +9,12 @@ import { PopoverComponent } from './components/popover/popover.component';
 import { ToggleComponent } from './components/toggle/toggle.component';
 import { TableComponent } from './components/table/table.component';
 import { SearchComponent } from './components/search/search.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { TabsComponent } from './components/tabs/tabs.component';
 
 
 @NgModule({
-  declarations: [AlertComponent, DialogComponent, LoadingPanelComponent, PopoverComponent, ToggleComponent, TableComponent, SearchComponent],
+  declarations: [AlertComponent, DialogComponent, LoadingPanelComponent, PopoverComponent, ToggleComponent, TableComponent, SearchComponent, BreadcrumbComponent, TabsComponent],
   imports: [
     CommonModule,
     CommonControlsRoutingModule
