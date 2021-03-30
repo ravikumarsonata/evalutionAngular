9372f483c100924fb3bdd8d6840558a2c0804589
Kaviyarasi M <kaviyarasi.m@sonata-software.com>
2021-03-30 15:42:07 +0530

    material dialog and json file

--- evaluation/ClientApp/src/app/components/mysurveys/mysurveys.component.ts
+++ evaluation/ClientApp/src/app/components/mysurveys/mysurveys.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 
 import { MatTableDataSource } from '@angular/material/table';
+import { DialogComponent } from 'src/app/shared/components/dialog/dialog.component';
 
 @Component({
   selector: 'app-mysurveys',
@@ -9,5 +11,14 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./mysurveys.component.css']
 })
 export class MysurveysComponent  {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
 }
