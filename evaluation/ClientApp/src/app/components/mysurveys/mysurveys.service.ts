85399865aa8353efa2dd793a1a0df901d0965812
Kaviyarasi M <kaviyarasi.m@sonata-software.com>
2021-04-05 15:00:30 +0530

    minor changes

--- evaluation/ClientApp/src/app/components/mysurveys/mysurveys.service.ts
+++ evaluation/ClientApp/src/app/components/mysurveys/mysurveys.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import * as data from '../mysurveys/configuration.json';
+import { employee } from './employee';
 @Injectable({
   providedIn: 'root'
 })
 export class MysurveysService {
-
+public employeeData :any =  data
   constructor(private http: HttpClient) { }
   getData() {
     console.info("calling...")
     return this.http
       .get('assets/employee.json')
+      //.get<employee[]>(this.employeeData)
     // .pipe(map(x => x));
   }
 }
