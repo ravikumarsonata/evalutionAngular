9fd8bd40d7de21e52cc6d21cb82ab4e32e473ba3
Ravikumar <ravikumarachari.s@sonata-software.com>
2021-04-05 13:49:57 +0530

    added questionnaire component

--- evaluation/ClientApp/src/app/components/mysurveys/mysurveys.service.ts
+++ evaluation/ClientApp/src/app/components/mysurveys/mysurveys.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import * as data from '../mysurveys/configuration.json';
+import * as data from './configuration.json';
 @Injectable({
   providedIn: 'root'
 })
