19538cbd8e8e51aad0a8d19ed8010d63aabd67ab
Ravikumar <ravikumarachari.s@sonata-software.com>
2021-04-07 16:14:18 +0530

    updated questionnaire component code commented old one

--- evaluation/ClientApp/src/app/components/questionnaire/questionnaire.module.ts
+++ evaluation/ClientApp/src/app/components/questionnaire/questionnaire.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { QuestionnaireRoutingModule } from './questionnaire-routing.module';
 import { QuestionnaireComponent } from './questionnaire.component';
+import { MaterialModule } from '../../material/material.module';
 
 
 @NgModule({
   declarations: [QuestionnaireComponent],
   imports: [
     CommonModule,
+    MaterialModule,
     QuestionnaireRoutingModule
   ]
 })
