e033a5cf240977b557ec6d59e731240849921ea9
Ravikumar <ravikumarachari.s@sonata-software.com>
2021-04-20 14:09:11 +0530

    updated service to questionnaire

--- evaluation/ClientApp/src/app/components/questionnaire/questionnaire.service.ts
+++ evaluation/ClientApp/src/app/components/questionnaire/questionnaire.service.ts
@@ -1,9 +1,19 @@
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionnaireService {
-
-  constructor() { }
+ /* public employeeData: any = data*/
+  constructor(private http: HttpClient) { }
+  getData() {
+    console.info("calling...")
+    return this.http
+      .get('assets/employee.json')
+    //.get<employee[]>(this.employeeData)
+    // .pipe(map(x => x));
+  }
 }
